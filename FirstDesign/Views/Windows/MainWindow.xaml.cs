38eb4053b2c8bb00b58ff5f3d1c5675ff7cd21a9
Flazer FAM <nicholas620@mail.ru>
2023-09-23 16:06:48 +0400

    Добавьте файлы проекта.

---
+++ FirstDesign/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstDesign.Views.Pages;
+
+namespace FirstDesign.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Content = new AuthPage();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Выход из приложения
+            Application.Current.Shutdown();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //try..catch обработчик ошибок
+            try
+            {
+                MainFrame.GoBack();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Нет страниц для перехода назад");
+            }
+        }
+    }
+}
